8b4ddc9c366775dc79408df655d0ec2c67a36c02
vilvapriyakoma <vilvapriyakoma@gmail.com>
2025-10-11 17:59:24 +0530

    11-10-2025

---
+++ page-1/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginBtn = document.getElementById("login");
+    const signupBtn = document.getElementById("signup");
+    loginBtn.addEventListener("click", function() {
+        window.location.href = "login.html"; 
+    });
+    signupBtn.addEventListener("click", function() {
+        window.location.href = "login.html"; 
+    });
+});
